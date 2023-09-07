e51cb85a0af76f5113662bc09110e678c805803e
Lucas França <lucasvpessoafranca@gmail.com>
2023-09-07 02:16:31 -0300

    feat:  add register route in ProductController

--- src/main/java/br/com/api/products/controllers/ProductController.java
+++ src/main/java/br/com/api/products/controllers/ProductController.java
@@ -1,7 +1,10 @@
 package br.com.api.products.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.api.products.model.ProductModel;
@@ -16,5 +19,12 @@ public class ProductController {
     @GetMapping("/listAll")
     public Iterable<ProductModel> listAll() {
         return ps.listAll();
+    }
+
+    @PostMapping("/register")
+
+    public ResponseEntity<?> register(@RequestBody ProductModel pm) {
+        return ps.register(pm);
+
     }
 }
