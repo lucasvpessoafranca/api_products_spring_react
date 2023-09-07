837900c9fb01ab8f217162c900dc0de4ea1da7d8
Lucas França <lucasvpessoafranca@gmail.com>
2023-09-07 02:04:12 -0300

    feat: add in service - findAll

--- src/main/java/br/com/api/products/services/ProductService.java
+++ src/main/java/br/com/api/products/services/ProductService.java
@@ -1,5 +1,19 @@
 package br.com.api.products.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.api.products.model.ProductModel;
+import br.com.api.products.repository.ProductRepository;
+
+@Service
 public class ProductService {
+
+    @Autowired
+    private ProductRepository pr;
+
+    public Iterable<ProductModel> listar() {
+        return pr.findAll();
+    }
 
 }
