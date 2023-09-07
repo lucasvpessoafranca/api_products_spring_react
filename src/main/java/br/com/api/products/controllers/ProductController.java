691f4a19f1c476e96c00a2f2f9f89eb34983a487
Lucas França <lucasvpessoafranca@gmail.com>
2023-09-07 02:01:55 -0300

    feat: config controller and ProductResponse

--- src/main/java/br/com/api/products/controllers/ProductController.java
+++ src/main/java/br/com/api/products/controllers/ProductController.java
@@ -1,8 +1,14 @@
 package br.com.api.products.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
 
+    @GetMapping("/")
+    public String rota() {
+
+        return "API FUNCIONANDO";
+    }
 }
